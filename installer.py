412083396147db9c6372120f6afa1be52d50a2a4
DiamondGotCat <diamondgotcat@gmail.com>
2025-06-16 22:00:36 +0900

    Support BuildV2

--- installer.py
+++ installer.py
@@ -35,28 +35,55 @@ def gatekeeperDisable(path: str):
 
 isGatekeeperCommandRequire = False
 system = platform.system()
+useV2 = "N"
 
 if system == "Darwin":
-    print("[blue]Detected Platform:[/blue] macOS (Darwin Kernel)")
-    print("[yellow][bold]NOTE:[/bold] macOS binaries currently only support arm64 (for Apple Silicon).[/yellow]")
-    downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-macos.zip"
-    originalBinaryName = "pylo-macos"
+    print("[blue]Detected Platform:[/blue] macOS")
+    print("[blue][AutoBuild V2] v14.3-r1 and later are compatible with Intel version of macOS.[/blue]")
+    useV2 = Prompt.ask("Would you like to use that?", choices=["y", "N"], default="N")
+    if useV2 == "N":
+        downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-macos.zip"
+        originalBinaryName = "pylo-macos"
+    else:
+        downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-macos-arm64.zip"
+        originalBinaryName = "pylo-macos-arm64"
     finalBinaryName = "pylo"
     isGatekeeperCommandRequire = True
-elif system == "Linux":
-    print("[blue]Detected Platform:[/blue] Linux")
-    print("[yellow][bold]NOTE:[/bold] Linux binaries currently only support amd64 (x86_64).[/yellow]")
-    downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-linux.zip"
-    originalBinaryName = "pylo-linux"
-    finalBinaryName = "pylo"
-elif system == "Windows":
-    print("[blue]Detected Platform:[/blue] Windows")
-    print("[yellow][bold]NOTE:[/bold] Windows binaries currently only support amd64 (x86_64).[/yellow]")
-    downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-windows.zip"
-    originalBinaryName = "pylo-windows.exe"
-    finalBinaryName = "pylo.exe"
 else:
-    print(f"[red][bold]Unsupported platform:[/bold] {system}[/red]")
+    print("[blue][AutoBuild V2]Starting from v14.3-r1, a new build structure is available.[/blue]")
+    useV2 = Prompt.ask("Would you like to use it?", choices=["y", "N"], default="N")
+
+    if useV2 == "N":
+        if system == "Linux":
+            print("[blue]Detected Platform:[/blue] Linux")
+            print("[yellow][bold]NOTE:[/bold] Linux binaries currently only support amd64 (x86_64).[/yellow]")
+            downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-linux.zip"
+            originalBinaryName = "pylo-linux"
+            finalBinaryName = "pylo"
+        elif system == "Windows":
+            print("[blue]Detected Platform:[/blue] Windows")
+            print("[yellow][bold]NOTE:[/bold] Windows binaries currently only support amd64 (x86_64).[/yellow]")
+            downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-windows.zip"
+            originalBinaryName = "pylo-windows.exe"
+            finalBinaryName = "pylo.exe"
+        else:
+            print(f"[red][bold]Unsupported platform:[/bold] {system}[/red]")
+    
+    else:
+        if system == "Linux":
+            print("[blue]Detected Platform:[/blue] Linux")
+            print("[yellow][bold]NOTE:[/bold] Linux binaries currently only support amd64 (x86_64).[/yellow]")
+            downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-linux-amd64.zip"
+            originalBinaryName = "pylo-linux-amd64"
+            finalBinaryName = "pylo"
+        elif system == "Windows":
+            print("[blue]Detected Platform:[/blue] Windows")
+            print("[yellow][bold]NOTE:[/bold] Windows binaries currently only support amd64 (x86_64).[/yellow]")
+            downloadURL = f"https://github.com/DiamondGotCat/Pylo/releases/download/{useTag}/pylo-windows-amd64.zip"
+            originalBinaryName = "pylo-windows-amd64.exe"
+            finalBinaryName = "pylo.exe"
+        else:
+            print(f"[red][bold]Unsupported platform:[/bold] {system}[/red]")
 
 print()
 
@@ -76,10 +103,17 @@ print("[blue bold]Install Infomation[/blue bold]")
 print("[blue]Platform:[/blue] " + systemFriendly)
 print("[blue]Version:[/blue] " + useTag)
 print("[blue]Download URL:[/blue] " + downloadURL)
+
 if systemFriendly == "macOS" and (isGatekeeperCommandRequire):
     print("[blue]Disable Gatekeeper:[/blue] Yes")
 elif systemFriendly == "macOS" and (not isGatekeeperCommandRequire):
     print("[blue]Disable Gatekeeper:[/blue] No")
+
+if useV2 == "N":
+    print("[blue]AutoBuild V2:[/blue] No")
+else:
+    print("[blue]AutoBuild V2:[/blue] Yes")
+
 print("[blue]Install to[/blue] " + installDir)
 confirm = Prompt.ask("Continue?", default="Y", choices=["Y", "n"])
 
@@ -91,31 +125,31 @@ print()
 with tempfile.TemporaryDirectory() as tmp_dir:
     zipPath = os.path.join(tmp_dir, "pylo.zip")
 
-    print(f"{downloadURL} -> {zipPath}")
+    print(f"(TASK) {downloadURL} -> {zipPath}")
     response = requests.get(downloadURL)
     response.raise_for_status()
     with open(zipPath, 'wb') as f:
         f.write(response.content)
 
-    print(f"{zipPath} -> {tmp_dir}/{originalBinaryName}")
+    print(f"(TASK) {zipPath} -> {tmp_dir}/{originalBinaryName}")
     with zipfile.ZipFile(zipPath, 'r') as zip_ref:
         zip_ref.extract(originalBinaryName, tmp_dir)
     extractedBinaryPath = os.path.join(tmp_dir, originalBinaryName)
     os.makedirs(installDir, exist_ok=True)
     finalBinaryPath = os.path.join(installDir, finalBinaryName)
 
-    print(f"{extractedBinaryPath} -> {finalBinaryPath}")
+    print(f"(TASK) {extractedBinaryPath} -> {finalBinaryPath}")
     shutil.copy2(extractedBinaryPath, finalBinaryPath)
 
-    print(f"{finalBinaryPath} (Non-Executable) -> {finalBinaryPath} (Executable)")
+    print(f"(TASK) {finalBinaryPath} (Non-Executable) -> {finalBinaryPath} (Executable)")
     os.chmod(finalBinaryPath, 0o755)
 
     if isGatekeeperCommandRequire:
-        print(f"{finalBinaryPath} -> Gatekeeper Verified List")
+        print(f"(TASK) {finalBinaryPath} -> Gatekeeper Verified List")
         gatekeeperDisable(finalBinaryPath)
         print("[blue]You might see a \"No such xattr: com.apple.quarantine\" error, but don't worry. It just means the quarantine has already been disabled.[/blue]")
 
-    print(f"Installed binary to: {finalBinaryPath}")
+    print(f"(TASK) Installed binary to: {finalBinaryPath}")
 
 print()
 
@@ -123,9 +157,9 @@ user_path = os.environ.get("PATH", "")
 if installDir not in user_path.split(os.pathsep):
     if system == "Windows":
         print()
-        print("[yellow bold]WARNING: ~/.local/bin is not in your PATH.[/yellow bold]")
+        print("[yellow bold]WARNING: ~\\.local\\bin is not in your PATH.[/yellow bold]")
         print("Please add the following to your Windows system environment variable PATH:")
-        print(f"{os.path.expanduser('~')}¥.local¥bin\n")
+        print(f"{os.path.expanduser('~')}\\.local\\bin\n")
     else:
         print()
         print("[yellow bold]WARNING: ~/.local/bin is not in your PATH.[/yellow bold]")
@@ -133,7 +167,7 @@ if installDir not in user_path.split(os.pathsep):
         print('export PATH="$HOME/.local/bin:$PATH"\n')
 else:
     if system == "Windows":
-        print("[blue bold]Your PATH includes ~¥.local¥bin. You're good to go![/blue bold]")
+        print("[blue bold]Your PATH includes ~\\.local\\bin. You're good to go![/blue bold]")
     else:
         print("[blue bold]Your PATH includes ~/.local/bin. You're good to go![/blue bold]")
 
